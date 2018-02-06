c470dfdb03681ac3b40395e0f4d2ce479158f49f
remco770 <remco@vanlent.net>
2018-02-06 19:06:21 +0100

    Fix issue with alarm, adding relais to config

--- python_scripts/alarm_inbraak.py
+++ python_scripts/alarm_inbraak.py
@@ -1,9 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(22, GPIO.OUT)
 
+GPIO.output(22, GPIO.LOW)
+time.sleep(.5)
 GPIO.output(22, GPIO.HIGH)
 
 time.sleep(10)
